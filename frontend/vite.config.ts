0819870b57dbca9058f28456eaf38f48763d362e
Joshua Farrington <joshua.l.farrington.civ@army.mil>
2022-08-23 10:43:23 -0500

    initial commit frontend backend docker-compose

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    },
+  },
+  plugins: [react()]
+})
---
+++ src/test/java/com/example/reactspringstarter/ReactSpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactspringstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
