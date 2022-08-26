2e4bdebdcf1d6358ec00f9af4ce789e2addb97ce
Joshua Farrington <joshua.l.farrington.civ@army.mil>
2022-08-25 22:16:36 -0500

    add AnotherPage component that gets text from the server and displays it

---
+++ frontend/src/AnotherPage.tsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Stack, Typography} from "@mui/material";
+import {grey} from "@mui/material/colors";
+
+const AnotherPage = () => {
+
+    const [textFromServer, setTextFromServer] = useState<string>('');
+
+    useEffect(() => {
+        axios.get("/api/sample").then(r => setTextFromServer(r.data));
+    }, []);
+
+    return (
+        <Stack gap={4}>
+            <Typography variant='h3'>
+                Another Page
+            </Typography>
+            <Typography variant='body1' color={grey[500]}>
+                {textFromServer}
+            </Typography>
+        </Stack>
+    );
+};
+
+export default AnotherPage;
