edc4264173c99a8981c178aa69d4da280162a188
Joshua Farrington <joshua.l.farrington.civ@army.mil>
2022-08-25 21:38:27 -0500

    add sample controller and test

---
+++ src/main/java/com/example/reactspringstarter/SampleController.java
@@ -0,0 +1,16 @@
+package com.example.reactspringstarter;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SampleController {
+
+    @GetMapping("/sample")
+    public ResponseEntity<String> sampleGetEndpoint(){
+        return ResponseEntity.ok("This came from the server");
+    }
+}
---
+++ src/test/java/com/example/reactspringstarter/SampleControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.reactspringstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class SampleControllerTest {
+
+    @Test
+    void sampleControllerTest() throws Exception {
+        MockMvc mvc = MockMvcBuilders.standaloneSetup(new SampleController()).build();
+
+        mvc.perform(get("/api/sample"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("This came from the server"));
+    }
+}
